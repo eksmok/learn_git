30154e06bbc891d907a5289169503c9febb347ab
eksmok <adamaly.shiraz@outlook.fr>
2022-07-17 10:32:09 +0200

    class1

---
+++ src/file1.py
@@ -0,0 +1,13 @@
+class Feature1:
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def method1(self):
+        pass
+
+    def method2(self):
+        pass
+
+    def method3(self):
+        pass 
